7e9523590463bc88c39a6abb6751c91927d9c474
bonface murangiri <bonnyrangi95@gmail.com>
2023-10-15 22:05:21 +0000

    user update

--- models/user.py
+++ models/user.py
@@ -14,11 +14,11 @@ class User(BaseModel):
         first_name (str): The user's first name.
         last_name (str): The user's last name.
     """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
         """Initialize a new User instance."""
         super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
